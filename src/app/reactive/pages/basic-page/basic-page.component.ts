1fdc789d4b6fddc5f9b7a9d74cfbfaa9e5f06fa8
jesusrobot0 <jesusrobot9000@icloud.com>
2024-08-29 00:52:56 -0500

    Añade el primer formGroup y lo conecta con el html

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class BasicPageComponent {
+  // public miForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   stock: new FormControl(0),
+  // });
 
+  // Esto es lo mismo que lo de arriba, solo que más limpio
+  // Pero por detras formbuiler hace lo mismo.
+  public myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: [''],
+      price: [0],
+      stock: [0],
+    });
+  }
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
