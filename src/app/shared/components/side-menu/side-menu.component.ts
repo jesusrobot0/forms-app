f7a7628ea59962e90ba0f4fb0722c9b62f6a37ae
jesusrobot0 <jesusrobot9000@icloud.com>
2024-08-28 23:33:35 -0500

    chore: Add side menu component and update app layout

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: ``,
+})
+export class SideMenuComponent {
+  public reactiveMenuItems: MenuItem[] = [
+    { title: 'Básicos', route: './reactive/basic' },
+    { title: 'Dinámicos', route: './reactive/dynamic' },
+    { title: 'Switches', route: './reactive/switches' },
+  ];
+
+  public authMenuItems: MenuItem[] = [{ title: 'Registro', route: './route' }];
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { SharedRoutingModule } from './shared-routing.module';
 
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SharedRoutingModule
-  ]
+  declarations: [SideMenuComponent],
+  imports: [CommonModule, SharedRoutingModule, RouterModule],
+  exports: [SideMenuComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
